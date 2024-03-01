dd9c01ba57ad44c48d48f1ea86d777abfc0f6dc2
JoanMendo <147327997+JoanMendo@users.noreply.github.com>
2024-03-01 12:17:55 +0100

    Add files via upload

--- Buscaminas/Buscaminas.js
+++ Buscaminas/Buscaminas.js
@@ -1,78 +1,130 @@
 
 class Tablero {
-    container; 
     altura;
     anchura;
-    minas;
     tablero;
+    minas;
     constructor(altura, anchura, minas) {
         this.altura = altura;
         this.anchura = anchura;
-        this.minas = minas;
         this.tablero = [];
-        this.container = document.getElementById('tablero');
+        this.minas = minas;
         this.generarTablero(altura, anchura);
+        this.colocarBombas(minas);
+
     }
+
+    getTablero() {
+        return this.tablero;
+
+    }
+
     generarTablero(altura, anchura) {
         for (let i = 0; i < altura; i++) {
-            let fila = document.createElement('div');
-            fila.classList.add('fila');
             this.tablero[i] = [];
             for (let j = 0; j < anchura; j++) {
                 let celda = new Celda(i, j);
-                fila.appendChild(celda.element);
                 this.tablero[i][j] = celda;
             }
-            container.appendChild(fila);
+        }
+    }
+    colocarBombas(minas) {
+        for (let i = 0; i < minas; i++) {
+            let x = Math.floor(Math.random() * this.altura);
+            let y = Math.floor(Math.random() * this.anchura);
+            if (this.tablero[x][y].hasBomba === true) {
+                i--;
+            } else {
+                this.tablero[x][y].hasBomba = true;
+            }
+        }
+    }
+    
+    desvelarTablero() {
+        alert('Has perdido');
+        for (let i = 0; i < this.altura; i++) {
+            for (let j = 0; j < this.anchura; j++) {
+
+                this.tablero[i][j].element.classList.remove('hidden');
+                this.tablero[i][j].element.classList.add('visible');
+
+            }
         }
     }
 }
 class Celda {
-posicionX;
-posicionY;
-hasBandera;
-hasBomba;
-esCasillaInicial;
-isDesvelada;
-numero;
-constructor(posicionX, posicionY) {
-    this.posicionX = posicionX;
-    this.posicionY = posicionY;
-    this.element = document.createElement('div');
-    this.element.classList.add('celda');
-    this.element.classList.add('hidden');
-    this.element.onclick = () => this.desvelarCelda();
-    this.element.onauxclick = () => this.element.classList.toggle('bandera');
+    posicionX;
+    posicionY;
+    hasBomba = false;
+    esCasillaInicial;
+    isDesvelada;
+    numero = 0;
+    constructor(posicionX, posicionY) {
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+        
     }
-    desvelarCelda() {
-
-        this.element.classList.remove('hidden');
-        this.element.classList.add('visible');
-        this.isDesvelada = true;
-    }
-}
-
-class Player {
     
 }
+
+class PlayerInteraction {
+
+    tableroMinas
+
+    constructor(altura, anchura, minas) {
+        this.tableroMinas = new Tablero(altura, anchura, minas);
+
+    }
+    checkTablero() {
+        
+        if (this.tableroMinas.altura < 8 || this.tableroMinas.anchura < 8 || this.tableroMinas.minas > (this.tableroMinas.altura * this.tableroMinas.anchura) * 0.33 || this.tableroMinas.altura > 24 || this.tableroMinas.anchura > 32 || this.tableroMinas.minas < 1) {
+            alert('La altura debe estar entre 8 y 24, la anchura entre 8 y 32 y el número de minas debe ser menor al 33% del total de casillas y mayor a 0');
+        }
+        else {
+            let startButton = document.getElementById('startButton');
+            startButton.disabled = true;
+            let container = document.getElementById('container');
+
+            for (let i = 0; i < this.tableroMinas.altura; i++) {
+                let fila = document.createElement('div');
+                fila.classList.add('fila');
+                for (let j = 0; j < this.tableroMinas.anchura; j++) {
+                    let casilla = this.tableroMinas.tablero[i][j];     
+                    casilla = document.createElement('div');
+                    casilla.classList.add('celda');
+                    casilla.classList.add('hidden');
+                    if (casilla.hasBomba === true) {
+                        casilla.classList.add('bomba');
+                    }
+                    casilla.onclick = () => {
+                        if (casilla.hasBomba && casilla.classList.contains('bandera') === false && casilla.classList.contains('visible') === false) {
+                            this.tableroMinas.desvelarTablero();
+                        }
+                        casilla.classList.remove('hidden');
+                        casilla.classList.add('visible');
+                        casilla.isDesvelada = true;
+                
+                    }
+                    casilla.onauxclick = () => casilla.classList.toggle('bandera');
+                    fila.appendChild(casilla); 
+                }
+                container.appendChild(fila);
+            }
+        }
+
+    }
+
+
+}
+
 function init() {
     let altura = document.getElementById('Alto').value;
     let anchura = document.getElementById('Ancho').value;
     let minas = document.getElementById('Minas').value;
-    let tablero = new Tablero(altura, anchura, minas);
-    console.log(si);
+    juego = new PlayerInteraction(altura, anchura, minas);
+    juego.checkTablero();
 }
 
-function checkValues() {
-    let altura = document.getElementById('Alto').value;
-    let anchura = document.getElementById('Ancho').value;
-    let minas = document.getElementById('Minas').value;
-    if (altura < 8 || anchura < 8 || minas > (altura * anchura) * 0.33 || altura > 24 || anchura > 32 || minas < 1) {
-        alert('La altura debe estar entre 8 y 24, la anchura entre 8 y 32 y el número de minas debe ser menor al 33% del total de casillas y mayor a 0');
-    } 
-    else {
-        let startButton = document.getElementById('startButton');
-        startButton.disabled = true;
-        init();
-    }
-}
+
+
+
