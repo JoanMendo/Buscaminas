561cd54bfaa56b7c99b2087fd2e416aac4e4cc13
JoanMendo <147327997+JoanMendo@users.noreply.github.com>
2024-03-13 11:33:13 +0100

    Add files via upload

---
+++ Buscaminas/Celda.js
@@ -0,0 +1,19 @@
+class Celda {
+    posicionX;
+    posicionY;
+    hasBomba = false;
+    esCasillaInicial;
+    isDesvelada = false;
+    numero = 0;
+    initialClick = false;
+    constructor(posicionX, posicionY) {
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+        this.esCasillaInicial = true;
+
+    }
+    getN() { //Devuelve el numero de la celda, por lo que he visto cuando se una un objeto como parametro de un metodo, no puedes hacer celda.numero
+        return this.numero;
+    }
+
+}
